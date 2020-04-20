6fc7254e0f2168a67f336be2ae30bd93b0ad6154
shaojiale <sjl66666666@gmail.com>
2020-04-20 15:08:22 +0800

    rebuild cleaner

---
+++ src/prototype/cleaner.go
@@ -0,0 +1,47 @@
+package prototype
+
+import (
+	"fmt"
+	"time"
+)
+
+type cleaner struct {
+	interval time.Duration
+	stop     chan bool
+	clean    func(*cache)
+}
+
+func (cl *cleaner) run(c *cache) {
+	ticker := time.Tick(cl.interval)
+	for {
+		select {
+		case <-ticker:
+			cl.clean(c)
+		case <-cl.stop:
+			return
+		}
+	}
+}
+
+func (cl *cleaner) stopNow() {
+	cl.stop <- true
+}
+
+func (cl *cleaner) setInterval(t time.Duration) error {
+	if t < 0 {
+		return fmt.Errorf("interval must greater than zero: %v", t)
+	}
+	cl.interval = t
+	return nil
+}
+
+func defaultCleaner(t time.Duration) *cleaner {
+	cl := &cleaner{
+		interval: t,
+		stop:     make(chan bool),
+		clean: func(c *cache) {
+			c.delExpired()
+		},
+	}
+	return cl
+}
