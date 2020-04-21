73e9457feb3536305ea6dc758c2995a6801d74fa
shaojiale <sjl66666666@gmail.com>
2020-04-21 18:14:36 +0800

    add a signal of stop

--- src/tailor/cleaner.go
+++ src/tailor/cleaner.go
@@ -8,16 +8,23 @@ import (
 type cleaner struct {
 	interval time.Duration
 	stop     chan bool
+	stopped  bool
 	clean    func(*cache)
 }
 
+func (cl *cleaner) isStopped() bool {
+	return cl.stopped
+}
+
 func (cl *cleaner) run(c *cache) {
+	cl.stopped = false
 	ticker := time.Tick(cl.interval)
 	for {
 		select {
 		case <-ticker:
 			cl.clean(c)
 		case <-cl.stop:
+			cl.stopped = true
 			return
 		}
 	}
@@ -39,9 +46,20 @@ func defaultCleaner(t time.Duration) *cleaner {
 	cl := &cleaner{
 		interval: t,
 		stop:     make(chan bool),
+		stopped:  false,
 		clean: func(c *cache) {
 			c.delExpired()
 		},
+	}
+	return cl
+}
+
+func newCleanerWithHandler(t time.Duration, handler func(c *cache)) *cleaner {
+	cl := &cleaner{
+		interval: t,
+		stop:     make(chan bool),
+		stopped:  false,
+		clean:    handler,
 	}
 	return cl
 }
