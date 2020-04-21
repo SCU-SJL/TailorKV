c0dab976f3787d53be912119bccbe6d9ad711b7e
shaojiale <sjl66666666@gmail.com>
2020-04-21 20:17:37 +0800

    add test of unlink

--- src/tailor/prototype_test.go
+++ src/tailor/prototype_test.go
@@ -9,6 +9,9 @@ import (
 
 var c = NewCache(NoExpiration, nil)
 
+var keys = []string{"name", "id", "address", "bigInt"}
+var vals = []interface{}{"sjl", "2017141461144", "Chengdu", math.MaxInt64}
+
 func TestCache_DelExpired(t *testing.T) {
 	c.AddDelHandler(func(key string, val interface{}) {
 		fmt.Printf("delete key:%s val:%v\n", key, val)
@@ -29,5 +32,28 @@ func printCache(c *Cache) {
 	fmt.Println("---------------------------")
 	for k, v := range c.exCache.items {
 		fmt.Println("key:", k, "val:", v)
+	}
+}
+
+func TestCache_Unlink(t *testing.T) {
+	c.AddDelHandler(func(key string, val interface{}) {
+		fmt.Printf("delete key:%s val:%v\n", key, val)
+	})
+	for i := 0; i < len(keys); i++ {
+		c.Set(keys[i], vals[i])
+	}
+	for _, key := range keys {
+		c.Unlink(key)
+	}
+	ticker := time.After(5 * time.Second)
+	<-ticker
+	for _, key := range keys {
+		v := c.Get(key)
+		if v != nil {
+			t.Errorf("%s is not deleted, val: %v", key, v)
+		}
+	}
+	if c.neCache.asyncQueue.Size() > 0 {
+		t.Errorf("async queue is not empty")
 	}
 }
