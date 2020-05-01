cc2fb47ffefa346f6bdff1e586eb34456ed7fd39
shaojiale <sjl66666666@gmail.com>
2020-05-01 18:32:18 +0800

    add protocol for tailor server

---
+++ src/protocol/protocol.go
@@ -0,0 +1,26 @@
+package protocol
+
+import (
+	"encoding/json"
+)
+
+type Protocol struct {
+	Op  byte   `json:"op"`
+	Key string `json:"key"`
+	Val string `json:"val,omitempty"`
+	Exp string `json:"exp,omitempty"`
+}
+
+func (p *Protocol) GetJsonBytes() ([]byte, error) {
+	jsonBytes, err := json.Marshal(*p)
+	return jsonBytes, err
+}
+
+func ConvertToProtocol(data []byte) (*Protocol, error) {
+	var p Protocol
+	err := json.Unmarshal(data, &p)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
