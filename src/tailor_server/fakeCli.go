aac230ecfea9ea01b3a136a8c60d0ff204e1596b
shaojiale <sjl66666666@gmail.com>
2020-05-02 17:15:57 +0800

    add test of get

--- src/tailor_server/fakeCli.go
+++ src/tailor_server/fakeCli.go
@@ -7,6 +7,20 @@ import (
 	"protocol"
 )
 
+const (
+	setex byte = iota
+	setnx
+	set
+	get
+	del
+	unlink
+	incr
+	incrby
+	ttl
+	keys
+	cnt
+)
+
 func main() {
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", "localhost:8448")
 	if err != nil {
@@ -16,15 +30,40 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	//testSetex(conn)
+	testGet(conn)
+}
+
+func testSetex(conn net.Conn) {
+
 	data := &protocol.Protocol{
-		Op:  0,
+		Op:  setex,
 		Key: "name",
 		Val: "Jack Ma",
-		Exp: "500",
+		Exp: "5000",
 	}
 	datagram, _ := data.GetJsonBytes()
 	_, _ = conn.Write(datagram)
 	errMsg := make([]byte, 1)
 	_, _ = conn.Read(errMsg)
-	fmt.Println(errMsg[0])
+	fmt.Println("[setex] errMsg =", errMsg[0])
+}
+
+func testGet(conn net.Conn) {
+	data := &protocol.Protocol{
+		Op:  get,
+		Key: "name",
+		Val: "",
+		Exp: "",
+	}
+	datagram, _ := data.GetJsonBytes()
+	_, _ = conn.Write(datagram)
+	errMsg := make([]byte, 1)
+	_, _ = conn.Read(errMsg)
+	fmt.Println("[get] errMsg = ", errMsg[0])
+	if errMsg[0] == 0 {
+		buf := make([]byte, 4096)
+		n, _ := conn.Read(buf)
+		fmt.Println("[get] name = ", string(buf[:n]))
+	}
 }
