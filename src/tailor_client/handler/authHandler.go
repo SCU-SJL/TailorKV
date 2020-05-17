eb18269890ce1ff943b3b6f41d2bdb61cc26d144
shaojiale <sjl66666666@gmail.com>
2020-05-17 17:02:01 +0800

    add AES login for cli.

---
+++ src/tailor_client/handler/authHandler.go
@@ -0,0 +1,96 @@
+package handler
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func auth(conn net.Conn) error {
+	ok, err := authorized(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !ok {
+		password := readAuth("password")
+		aesKey := readAuth("AES key")
+		password = AesEncrypt(password, aesKey)
+
+		_, err = conn.Write([]byte(password))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		resp := make([]byte, 1)
+		_, err = conn.Read(resp)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if resp[0] != 0 {
+			return errors.New("wrong password")
+		}
+	}
+	return nil
+}
+
+func readAuth(param string) string {
+	in := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("Enter the %s: ", param)
+
+	input, err := in.ReadString('\n')
+
+	for err != nil {
+		fmt.Println("invalid input")
+		fmt.Printf("Enter %s again: ", param)
+		input, err = in.ReadString('\n')
+	}
+	input = strings.Replace(input, "\r\n", "", -1)
+	input = strings.Replace(input, "\n", "", -1)
+	return input
+}
+
+func authorized(conn net.Conn) (bool, error) {
+	buf := make([]byte, 1)
+	_, err := conn.Read(buf)
+	if err != nil {
+		return false, err
+	}
+	if buf[0] == 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+func AesEncrypt(orig string, key string) string {
+	origData := []byte(orig)
+	k := []byte(key)
+
+	block, err := aes.NewCipher(k)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockSize := block.BlockSize()
+	origData = PKCS7Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, k[:blockSize])
+	encrypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(encrypted, origData)
+
+	return base64.StdEncoding.EncodeToString(encrypted)
+}
+
+func PKCS7Padding(cipherText []byte, blockSize int) []byte {
+	padding := blockSize - len(cipherText)%blockSize
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(cipherText, padText...)
+}
